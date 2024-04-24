0046be533ec2e7ac4d078e94107916a3dff43ada
Shcoobz <a.boeckmann23@gmail.com>
2024-04-24 19:46:09 +0200

    implemented App

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,26 @@
-function App() {
+import { useRef } from 'react';
+import ImageLoader from './components/ImageLoader';
+import ImageContainer from './components/ImageContainer';
+import useFetchPhotos from './hooks/useFetchPhotos';
+import useInfiniteScroll from './hooks/useInfiniteScroll';
+
+const App = () => {
+  const { photos, loading, fetchPhotos } = useFetchPhotos();
+  const loaderRef = useRef(null);
+
+  useInfiniteScroll(loaderRef, () => {
+    if (!loading) {
+      fetchPhotos();
+    }
+  });
+
   return (
-    <div className='App'>
-      <h1>Hello</h1>
+    <div ref={loaderRef}>
+      <h1>Unsplash API - Infinite Scroll</h1>
+      {loading && <ImageLoader />}
+      <ImageContainer photos={photos} />
     </div>
   );
-}
+};
 
 export default App;
